733e6bc52dcc65796b4be0fb727c79a6a01b3b7c
404-Sandra <2425401619@live.gctu.edu.gh>
2026-05-30 09:47:49 +0000

    Assignment 2 (initials.py) submission

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Sandra Opokua Affoh
+# STUDENT ID: 2425401619
+# Email: akosuabae93@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Affoh Sandra Opokua, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
+    # This function prints my initials in stylized large letters.
+    print()
+    print("...SOA")
+    print()
+    
+    print(".....SSSSSSSSSS............OOOO.................AA..........")
+    print("....SSSSSSSSSSS..........OOOOOOOO..............AAAA.........")
+    print("...SSSSS...............OOO......OOO.........AAAA..AAAA......")
+    print("...SSSSS...............OO........OO........AAAA....AAAA.....")
+    print(".....SSSSSS............OO........OO........AAAAAAAAAAAA.....")
+    print(".......SSSSSS..........OO........OO........AAAAAAAAAAAA.....")
+    print("...........SSSS........OO........OO........AAA......AAA.....")
+    print("...........SSSS........OOO......OOO........AAA......AAA.....")
+    print("...SSSSSSSSSSS....**.....OOOOOOOO.....**...AAA......AAA...**")
+    print("...SSSSSSSSSS.....**.......OOOO.......**...AAA......AAA...**")
+    
+    print()
 
 main()
